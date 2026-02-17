396c6553026b98329db29b99dc1b0bbc5ca118e6
skpatrickue-73 <skpatrickue@gmail.com>
2026-02-17 13:18:20 +0100

    Entwurf für Backend: Notiz App, Grundstruktur + kleine Korrektur

--- __backend/entwurf.py
+++ __backend/entwurf.py
@@ -3,19 +3,52 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Note(BaseModel):
+class Nutzer(BaseModel):
     id: int
-    titel: str
-    content: str
+    name: str
+    passwort: int
+    board_liste: list
 
-notes = []
+class Board(BaseModel):
+    id: int
+    name: str
+    notizen_liste: list
+
+class Notiz(BaseModel):
+    id: int
+    name: str
+    inhalt: str
+
+nutzer_liste = []
+board_liste = []
+notiz_liste = []
+
+@app.get("/")
+def root():
+    return {"title": "Notiz App", "body": "Willkommen"} 
+
+@app.post("/neuer_nutzer")
+def neuer_nutzer(nutzer: Nutzer):
+    nutzer_liste.append(nutzer)
+    return {"message": "Nutzer erstellt"}
+
+@app.get("/nutzer")
+def get_nutzer():
+    return nutzer_liste
+@app.post("/neues_board")
+def neues_board(board: Board):
+    board_liste.append(board)
+    return {"message": "Board erstellt"}
+
+@app.get("/boards")
+def get_boards():
+    return board_liste
+
+@app.post("/neue_notiz")
+def neue_notiz(notiz: Notiz):
+    notiz_liste.append(notiz)
+    return {"message": "Notiz erstellt"}
 
 @app.get("/notizen")
-def get_notes():
-    print("Hole alle Notizen")
-    return notes
-
-@app.post("/notiz_hinzufuegen")
-def add_note(note: Note):
-    notes.append(note)
-    return {"message": "Notiz hinzugefügt"}
+def get_notizen():
+    return notiz_liste
