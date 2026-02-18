3c7a8a5376b04ffa3af69fc15e5a673bbfb8178a
skpatrickue-73 <skpatrickue@gmail.com>
2026-02-18 10:25:37 +0100

    Neue Datei: Passwort Code (128 Bit Version)

--- __backend/passwort_code.py
+++ __backend/passwort_code.py
@@ -1,7 +1,7 @@
 import hashlib
 
 
-def encrpyt_128(passwort: str) -> str:
+def encrypt_128(passwort: str) -> str:
     hash_object = hashlib.md5(passwort.enconde())
     encrypted = hash_object.hexdigets()
-    return encrpyted
+    return encrypted
