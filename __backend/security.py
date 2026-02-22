4a631a07633237f4bc3a3e542d21083888fa69cc
it-dennis <post@webkuchenhaus.de>
2026-02-22 21:09:58 +0100

    security.py angepasst, passlib, Bruteforce Schutz

--- __backend/security.py
+++ __backend/security.py
@@ -1,7 +1,10 @@
 from passlib.context import CryptContext
 
+#Passwort anlegen und verifizieren, passlib sorgt dafür, dass die Passwörter sicher gehasht werden 
+#und nicht im Klartext in der Datenbank liegen. 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# Hilfsfunktionen für Passwort-Hashing und -Verifizierung
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
